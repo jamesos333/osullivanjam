7e12ea6822685b4ded9d6f5f936943404504775f
James <>
2024-01-05 21:51:57 -0500

    various major and minor layout and theme improvements

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
-var scrollingText = "YOU ARE NOW EXITING THE FREE WORLD - YOUR IP ADDRESS HAS BEEN LOGGED AND YOUR INFORMATION "
-    + "HAS BEEN SENT TO ALL GOVERNMENTS WORLDWIDE - BY CONNECTING TO THIS WEBSITE YOU GIVE US PERMISSION "
-    + "TO REPOSSESS EVERY ORGAN IN YOUR BODY";
+var scrollingText = "- YOUR IP ADDRESS HAS BEEN LOGGED AND WILL BE HELD INDEFINITELY - <span style=\"color: #ff0000\">YOU ARE NOW ENTERING THE FREE WORLD</span>"
+    + " - BY CONNECTING TO THIS WEBSITE YOU REVOKE OWNERSHIP OF EVERY ORGAN IN YOUR BODY";
 
 // adds scrolling text content
 const scroll = document.querySelectorAll('[id=marqueetext]');
-console.log(scroll.length);
-scroll[0].textContent = scrollingText;
-scroll[1].textContent = scrollingText;
+console.log(scroll);
+document.querySelectorAll('[id=marqueetext]')[0].innerHTML = scrollingText;
+document.querySelectorAll('[id=marqueetext]')[1].innerHTML = scrollingText;
