aa22abf79345baf3f94efde33d2ef267b3b387c6
jamesos333 <osullivj2@newpaltz.edu>
2024-01-19 17:37:16 -0500

    created a 404 page, fixed a safari glitch, and edited quote css

--- elements/header.php
+++ elements/header.php
@@ -1,26 +1,28 @@
 <div id="container">
     <div id="header">
-        <div id="name">
-            <div class="row">
-                <img src="img/letters/j.gif" alt="j">
-                <img src="img/letters/a.gif" alt="a">
-                <img src="img/letters/m.gif" alt="m">
-                <img src="img/letters/e.gif" alt="e">
-                <img src="img/letters/s.gif" alt="s">
+        <a href="/">
+            <div id="name">
+                <div class="row">
+                    <img src="img/letters/j.gif" alt="j">
+                    <img src="img/letters/a.gif" alt="a">
+                    <img src="img/letters/m.gif" alt="m">
+                    <img src="img/letters/e.gif" alt="e">
+                    <img src="img/letters/s.gif" alt="s">
+                </div>
             </div>
-        </div>
-        <div id="name">
-            <div class="row">
-                <img src="img/letters/o.gif" alt="o">
-                <img src="img/letters/s.gif" alt="s">
-                <img src="img/letters/u.gif" alt="u">
-                <img src="img/letters/l.gif" alt="l">
-                <img src="img/letters/l.gif" alt="l">
-                <img src="img/letters/i.gif" alt="i">
-                <img src="img/letters/v.gif" alt="v">
-                <img src="img/letters/a.gif" alt="a">
-                <img src="img/letters/n.gif" alt="n">
+            <div id="name">
+                <div class="row">
+                    <img src="img/letters/o.gif" alt="o">
+                    <img src="img/letters/s.gif" alt="s">
+                    <img src="img/letters/u.gif" alt="u">
+                    <img src="img/letters/l.gif" alt="l">
+                    <img src="img/letters/l.gif" alt="l">
+                    <img src="img/letters/i.gif" alt="i">
+                    <img src="img/letters/v.gif" alt="v">
+                    <img src="img/letters/a.gif" alt="a">
+                    <img src="img/letters/n.gif" alt="n">
+                </div>
             </div>
-        </div>
+        </a>
     </div>
 </div>
