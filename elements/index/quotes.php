0396e64fd69f17df02f69d946f0c59ab9eadf6a1
jamesos333 <osullivj2@newpaltz.edu>
2024-04-08 15:43:43 -0400

    new quote

--- elements/index/quotes.php
+++ elements/index/quotes.php
@@ -3,6 +3,7 @@ $quoteArray = array(
     "an official language of univeral separation",
     "a labyrinth with no exit, a maze with no prize",
     "they spoke through my body in the form of an ominous prophecy",
+    "i no longer look for meaning in things. it's not my responsibility",
     "bitch you're not real, you're in my system"
 );
 
