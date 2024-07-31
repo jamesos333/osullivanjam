6c5ec65725f7d20d16519bab918884c400120a81
jamesos333 <osullivj2@newpaltz.edu>
2024-07-31 18:31:17 -0400

    refactored the template to use ternary operators in some places

--- modules/template/bottom.php
+++ modules/template/bottom.php
@@ -2,13 +2,13 @@
     date_default_timezone_set("America/New_York");
     $date = date("m-d-y h:i:sa");
     $copyrightPath = HTML_PATH . 'copyright.html';
-    $copyrightString = getFileTextContent($copyrightPath);
-    $copyrightString = str_replace('DATE_HERE', $date, $copyrightString);
+    $copyrightString = str_replace('%DATE%', $date, getFileTextContent($copyrightPath));
 ?>
 
     </div>
     <div class="copyright">
         <?=$copyrightString?>
     </div>
+    <script>0</script>
 </body>
 </html>
--- modules/template/top.php
+++ modules/template/top.php
@@ -1,10 +1,8 @@
 <?php
-    $htmxLink = "<script src='https://unpkg.com/htmx.org@2.0.0' integrity='sha384-wS5l5IKJBvK6sPTKa2WZ1js3d947pvWXbPJ1OmWfEuxLgeHcEbjUUA5i9V5ZkpCw' crossorigin='anonymous'></script>";
-    $isIndex = strcmp($pageName, 'index') == 0;
-    $title = 'osullivanjam.es';
-    if(!$isIndex) {
-        $title .= ': ' . $pageName;
-    }
+$htmxLink = "<script src='https://unpkg.com/htmx.org@2.0.0' integrity='sha384-wS5l5IKJBvK6sPTKa2WZ1js3d947pvWXbPJ1OmWfEuxLgeHcEbjUUA5i9V5ZkpCw' crossorigin='anonymous'></script>";
+$isIndex = strcmp($pageName, 'index') == 0;
+$title = 'osullivanjam.es' . (!$isIndex ? ': ' . $pageName : "");
+
 ?>
 
 <!DOCTYPE html>
@@ -14,15 +12,15 @@
     <meta charset="UTF-8">
     <meta name="description" content="osullivanjam.es">
     <meta name="theme-color" content="black" />
-    <link rel="icon" type="image/x-icon" href="<?=IMAGE_PATH . 'icon.png';?>">
-    <link rel="stylesheet" href="<?= CSS_PATH . $cssName . '.css?v=' . VERSION?>">
-    <?php if(isset($useHtmx) && $useHtmx){ echo $htmxLink; }?>
-    <title><?= $title?></title>
+    <link rel="icon" type="image/x-icon" href="<?= IMAGE_PATH . 'icon.png'; ?>">
+    <link rel="stylesheet" href="<?= CSS_PATH . $cssName . '.css?v=' . VERSION ?>">
+    <?= (isset($useHtmx) && $useHtmx) ? $htmxLink : "" ?>
+    <title><?= $title ?></title>
 </head>
 
 <body>
     <script>0</script>
     <?php include ELEMENTS_PATH . 'header.php'; ?>
-    <?php if($isIndex){ include ELEMENTS_PATH_INDEX . 'scrollingtext.php'; }?>
+    <?php $isIndex ? include ELEMENTS_PATH_INDEX . 'scrollingtext.php' : ''; ?>
     <div id="content">
-        <?php if(!$isIndex){ include ELEMENTS_PATH . 'navbar.php'; }?>
+        <?php !$isIndex ? include ELEMENTS_PATH . 'navbar.php' : ''; ?>
