291ad986c039e5f4d72492096c6b2a15af19c3c9
jamesos333 <osullivj2@newpaltz.edu>
2024-09-17 23:19:00 -0400

    updated template so that only main pages will be indexed by search engines

--- modules/template/top.php
+++ modules/template/top.php
@@ -6,9 +6,12 @@
 // --useHtmx
 // --description
 // --additionalHeader
-$htmxLink = "<script src='https://unpkg.com/htmx.org@2.0.2' integrity='sha384-Y7hw+L/jvKeWIRRkqWYfPcvVxHzVzn5REgzbawhxAuQGwX1XWe70vji+VSeHOThJ' crossorigin='anonymous' defer></script>";
 $isIndex = strcmp($pageName, 'index') == 0;
 $title = 'osullivanjam.es' . (!$isIndex ? ': ' . $pageName : "");
+
+$htmxLink = "<script src='https://unpkg.com/htmx.org@2.0.2' integrity='sha384-Y7hw+L/jvKeWIRRkqWYfPcvVxHzVzn5REgzbawhxAuQGwX1XWe70vji+VSeHOThJ' crossorigin='anonymous' defer></script>";
+$robotsMeta = "<meta name='robots' content='index, follow'>";
+$descriptionMeta = "<meta name='description' content='" . (isset($description) ? $description : $title) . "'>";
 ?>
 
 <!DOCTYPE html>
@@ -16,19 +19,20 @@ $title = 'osullivanjam.es' . (!$isIndex ? ': ' . $pageName : "");
 
 <head>
     <meta charset="UTF-8">
-    <meta name="robots" content="index, follow">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="theme-color" content="black" />
-    <meta name="description" content="<?= isset($description) ? $description : $title ?>">
     <link rel="icon" type="image/x-icon" href="<?= IMAGE_PATH . 'icon.png'; ?>">
     <link rel="stylesheet" href="<?= CSS_PATH . $cssName . '.css?v=' . VERSION ?>">
-    <?= (isset($useHtmx) && $useHtmx) ? $htmxLink : "" ?>
+    <?= isset($useHtmx) && $useHtmx ? $htmxLink : "" ?>
+    <?= count($_GET) >= 1 ? '' : ($robotsMeta . $descriptionMeta) ?>
     <?= isset($additionalHeader) ? $additionalHeader : "" ?>
     <title><?= $title ?></title>
 </head>
 
 <body style="background-color: black;">
-    <script>0</script>
+    <script>
+        0
+    </script>
     <?php include ELEMENTS_PATH . 'header.php'; ?>
     <?php $isIndex ? include ELEMENTS_PATH_INDEX . 'scrollingtext.php' : ''; ?>
     <div id="content">
