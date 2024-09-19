dd636162e7e3dc6c74e9e7ab6371659c1d64fb5e
jamesos333 <osullivj2@newpaltz.edu>
2024-09-18 22:02:41 -0400

    attempting to fix an issue with forwarded links from my other domain

--- modules/template/top.php
+++ modules/template/top.php
@@ -21,7 +21,6 @@ $htmxLink = "<script src='https://unpkg.com/htmx.org@2.0.2' integrity='sha384-Y7
     <meta name="theme-color" content="black" />
     <meta property="og:type" content="website">
     <meta property="og:title" content="osullivanjam.es">
-    <meta property="og:url" content="https://osullivanjam.es">
     <meta property="og:image" content="<?= "https://osullivanjam.es" . IMAGE_PATH_GLOBAL . "scaryface.jpg" ?>">
     <meta property="og:description" content="<?= $descriptionText ?>">
     <meta name='description' content='<?= $descriptionText ?>'>
