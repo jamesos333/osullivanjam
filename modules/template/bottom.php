6fc99c21b22bdca04e1570c4fe5a73147ac8bdbb
jamesos333 <osullivj2@newpaltz.edu>
2024-11-08 19:39:31 -0500

    will now display version in copyright disclaimer footer

--- modules/template/bottom.php
+++ modules/template/bottom.php
@@ -1,8 +1,12 @@
 <?php
-    date_default_timezone_set("America/New_York");
-    $date = date("m-d-y h:i:sa");
-    $copyrightPath = HTML_PATH . 'copyright.html';
-    $copyrightString = str_replace('%DATE%', $date, getFileTextContent($copyrightPath));
+date_default_timezone_set("America/New_York");
+$date = date("m-d-y h:i:sa");
+$copyrightPath = HTML_PATH . 'copyright.html';
+$copyrightString = str_replace(
+    array("%VERSION%", "%DATE%"),
+    array(VERSION, $date),
+    getFileTextContent($copyrightPath)
+);
 ?>
 
     </div>
