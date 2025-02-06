a23b53af6fc3903db9e9ca77499d296273612748
jamesos333 <osullivj2@newpaltz.edu>
2025-02-05 20:55:13 -0500

    replaced lorem ipsum with real blurb

--- views/communism.php
+++ views/communism.php
@@ -13,10 +13,13 @@ $description = "the doctrine of the liberation of the proletariat";
 </div>
 
 <div class="subtitle-container">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est recusandae nostrum nihil? Iure iusto cumque,
-    commodi ipsam quia est accusamus labore voluptas quaerat sit numquam praesentium minus? Dolores, unde iusto.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam eius numquam magnam sit consequuntur unde
-    sapiente veniam.
+    Don't be afraid comrade!! You have reached the osullivanjam.es center for marxist-leninist 
+    studies. The topic has been of great interest to me for the past few years and I have been 
+    devoting a lot of my personal time towards understanding marxist theory. The purpose of this 
+    page is twofold: one is to keep track of what I have read and store my thoughts on the readings, 
+    and secondly is to maintain a list of beginner readings for those who are just getting started 
+    with marxist theory. Feel free to peruse and read at your own pace and do not be afraid to use 
+    supplemental resources and read slowly. Good luck comrade! And fight on!!
 </div>
 
 <div class="section-container">
