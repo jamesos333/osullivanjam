4372aba2d57b2ad9ec11e1b32f71a22d7fb4c597
jamesos333 <osullivj2@newpaltz.edu>
2025-08-07 19:43:35 -0400

    further clean up binaural js

--- files/js/binaural.js
+++ files/js/binaural.js
@@ -1,92 +1,83 @@
 var playing = false;
-
 var context;
 var panLeftNode;
 var panRightNode;
 var gain;
-
-var osc1Frequency = 440;
 var osc1;
-
-var osc2Frequency = 447;
 var osc2;
-
 var timerId;
-
 var volume = 0.5;
-
 var isSafari = false;
 
 function togglePlay() {
-    if (this.playing === false) {
-        var AudioContext =
-            window.AudioContext || // Default
-            window.webkitAudioContext || // Safari and old versions of Chrome
-            false;
-        if (window.webkitAudioContext) {
-            this.isSafari = true;
-        }
-        if (AudioContext) {
-            var ctx = new AudioContext();
-        } else {
-            alert(
-                "Sorry, but the Web Audio API is not supported by your browser. Please, consider upgrading to the latest version or downloading Google Chrome or Mozilla Firefox"
-            );
-        }
-
-        var context = this.context || ctx;
-        this.context = context;
-
-        this.panLeftNode = this.isSafari
-            ? context.createPanner()
-            : context.createStereoPanner();
-        this.panRightNode = this.isSafari
-            ? context.createPanner()
-            : context.createStereoPanner();
-
-        if (isSafari) {
-            panLeftNode.panningModel = "equalpower";
-            panRightNode.panningModel = "equalpower";
-            panLeftNode.setPosition(1, 0, 0);
-            panRightNode.setPosition(-1, 0, 0);
-        } else {
-            this.panLeftNode.pan.value = -1;
-            this.panRightNode.pan.value = 1;
-        }
-
-        updateVolume(document.getElementById("volumeSlider").value);
-
-        this.osc1Freq = document.getElementById("freq1").value;
-        this.osc1 = context.createOscillator();
-        this.osc1.frequency.value = osc1Freq;
-
-        this.osc2Freq = document.getElementById("freq2").value;
-        this.osc2 = context.createOscillator();
-        this.osc2.frequency.value = osc2Freq;
-
-        this.gain = context.createGain();
-        this.gain.gain.setValueAtTime(0.001, context.currentTime);
-
-        this.osc1.connect(this.panLeftNode);
-        this.osc2.connect(this.panRightNode);
-        this.panLeftNode.connect(this.gain);
-        this.panRightNode.connect(this.gain);
-        this.gain.connect(context.destination);
-        this.osc1.start(0);
-        this.osc2.start(0);
-        this.gain.gain.exponentialRampToValueAtTime(
-            this.volume,
-            context.currentTime + 2
-        );
-        this.playing = true;
-
-        var minsToPlay = document.getElementById("timerMins").value;
-        timer(minsToPlay);
-        document.getElementById("startButton").innerHTML = "Stop";
-        document.getElementById("timerMins").disabled = true;
-    } else {
+    if (this.playing === true) {
         stop();
+        return;
     }
+    var AudioContext =
+        window.AudioContext || // Default
+        window.webkitAudioContext || // Safari and old versions of Chrome
+        false;
+    if (window.webkitAudioContext) {
+        this.isSafari = true;
+    }
+    if (AudioContext) {
+        var ctx = new AudioContext();
+    } else {
+        alert(
+            "Sorry, but the Web Audio API is not supported by your browser. Please, consider upgrading to the " +
+                "latest version or downloading Google Chrome or Mozilla Firefox."
+        );
+    }
+
+    var context = this.context || ctx;
+    this.context = context;
+
+    this.panLeftNode = this.isSafari
+        ? context.createPanner()
+        : context.createStereoPanner();
+    this.panRightNode = this.isSafari
+        ? context.createPanner()
+        : context.createStereoPanner();
+
+    if (isSafari) {
+        panLeftNode.panningModel = "equalpower";
+        panRightNode.panningModel = "equalpower";
+        panLeftNode.setPosition(1, 0, 0);
+        panRightNode.setPosition(-1, 0, 0);
+    } else {
+        this.panLeftNode.pan.value = -1;
+        this.panRightNode.pan.value = 1;
+    }
+
+    updateVolume(document.getElementById("volumeSlider").value);
+
+    this.osc1 = context.createOscillator();
+    this.osc1.frequency.value = document.getElementById("freq1").value;
+
+    this.osc2 = context.createOscillator();
+    this.osc2.frequency.value = document.getElementById("freq2").value;
+
+    this.gain = context.createGain();
+    this.gain.gain.setValueAtTime(0.001, context.currentTime);
+
+    this.osc1.connect(this.panLeftNode);
+    this.osc2.connect(this.panRightNode);
+    this.panLeftNode.connect(this.gain);
+    this.panRightNode.connect(this.gain);
+    this.gain.connect(context.destination);
+    this.osc1.start(0);
+    this.osc2.start(0);
+    this.gain.gain.exponentialRampToValueAtTime(
+        this.volume,
+        context.currentTime + 2
+    );
+    this.playing = true;
+
+    var minsToPlay = document.getElementById("timerMins").value;
+    timer(minsToPlay);
+    document.getElementById("startButton").innerHTML = "Stop";
+    document.getElementById("timerMins").disabled = true;
 }
 
 async function stop() {
@@ -112,18 +103,16 @@ async function stop() {
     setCountdownTextFromSeconds(0);
 }
 
-function updateFrequency(newFreq, oscillator) {
+function updateFrequency(newFreq, oscillatorId) {
     var correctedFreq = Math.min(Math.max(parseInt(newFreq), 1), 1000);
-    if (oscillator === 1) {
-        this.osc1Freq = correctedFreq;
+    if (oscillatorId === 1) {
         if (this.playing === true) this.osc1.frequency.value = correctedFreq;
-        document.getElementById("frequencyText1").value = this.osc1Freq;
-        document.getElementById("freq1").value = this.osc1Freq;
-    } else if (oscillator === 2) {
-        this.osc2Freq = correctedFreq;
+        document.getElementById("frequencyText1").value = correctedFreq;
+        document.getElementById("freq1").value = correctedFreq;
+    } else if (oscillatorId === 2) {
         if (this.playing === true) this.osc2.frequency.value = correctedFreq;
-        document.getElementById("frequencyText2").value = this.osc2Freq;
-        document.getElementById("freq2").value = this.osc2Freq;
+        document.getElementById("frequencyText2").value = correctedFreq;
+        document.getElementById("freq2").value = correctedFreq;
     }
 }
 
