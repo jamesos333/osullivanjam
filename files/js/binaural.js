9c07b9829c27e3369c73ece59cd6aa379b7877a8
jamesos333 <osullivj2@newpaltz.edu>
2025-08-07 20:02:58 -0400

    further refactored binaural js

--- files/js/binaural.js
+++ files/js/binaural.js
@@ -1,81 +1,60 @@
-var playing = false;
-var context;
-var panLeftNode;
-var panRightNode;
-var gain;
-var osc1;
-var osc2;
-var timerId;
-var volume = 0.5;
-var isSafari = false;
+let playing = false;
+let context;
+let gain;
+let osc1, osc2;
+let panLeft, panRight;
+let timerId;
+let volume = 0.5;
 
 function togglePlay() {
-    if (this.playing === true) {
+    if (playing) {
         stop();
         return;
     }
-    var AudioContext =
-        window.AudioContext || // Default
-        window.webkitAudioContext || // Safari and old versions of Chrome
-        false;
-    if (window.webkitAudioContext) {
-        this.isSafari = true;
-    }
-    if (AudioContext) {
-        var ctx = new AudioContext();
-    } else {
-        alert(
-            "Sorry, but the Web Audio API is not supported by your browser. Please, consider upgrading to the " +
-                "latest version or downloading Google Chrome or Mozilla Firefox."
-        );
+
+    if (!context) {
+        const AudioContext = window.AudioContext || window.webkitAudioContext;
+        if (!AudioContext) {
+            alert("Web Audio API is not supported in this browser :(");
+            return;
+        }
+        context = new AudioContext();
     }
 
-    var context = this.context || ctx;
-    this.context = context;
+    const isSafari = !!window.webkitAudioContext;
 
-    this.panLeftNode = this.isSafari
-        ? context.createPanner()
-        : context.createStereoPanner();
-    this.panRightNode = this.isSafari
-        ? context.createPanner()
-        : context.createStereoPanner();
+    panLeft = isSafari ? context.createPanner() : context.createStereoPanner();
+    panRight = isSafari ? context.createPanner() : context.createStereoPanner();
 
     if (isSafari) {
-        panLeftNode.panningModel = "equalpower";
-        panRightNode.panningModel = "equalpower";
-        panLeftNode.setPosition(1, 0, 0);
-        panRightNode.setPosition(-1, 0, 0);
+        panLeft.panningModel = "equalpower";
+        panRight.panningModel = "equalpower";
+        panLeft.setPosition(1, 0, 0);
+        panRight.setPosition(-1, 0, 0);
     } else {
-        this.panLeftNode.pan.value = -1;
-        this.panRightNode.pan.value = 1;
+        panLeft.pan.value = -1;
+        panRight.pan.value = 1;
     }
 
-    updateVolume(document.getElementById("volumeSlider").value);
+    osc1 = context.createOscillator();
+    osc1.frequency.value = parseInt(document.getElementById("freq1").value, 10);
+    osc2 = context.createOscillator();
+    osc2.frequency.value = parseInt(document.getElementById("freq2").value, 10);
 
-    this.osc1 = context.createOscillator();
-    this.osc1.frequency.value = document.getElementById("freq1").value;
+    gain = context.createGain();
+    gain.gain.setValueAtTime(0.001, context.currentTime);
 
-    this.osc2 = context.createOscillator();
-    this.osc2.frequency.value = document.getElementById("freq2").value;
+    osc1.connect(panLeft).connect(gain);
+    osc2.connect(panRight).connect(gain);
+    gain.connect(context.destination);
+    osc1.start();
+    osc2.start();
 
-    this.gain = context.createGain();
-    this.gain.gain.setValueAtTime(0.001, context.currentTime);
-
-    this.osc1.connect(this.panLeftNode);
-    this.osc2.connect(this.panRightNode);
-    this.panLeftNode.connect(this.gain);
-    this.panRightNode.connect(this.gain);
-    this.gain.connect(context.destination);
-    this.osc1.start(0);
-    this.osc2.start(0);
-    this.gain.gain.exponentialRampToValueAtTime(
-        this.volume,
-        context.currentTime + 2
-    );
-    this.playing = true;
-
-    var minsToPlay = document.getElementById("timerMins").value;
+    gain.gain.exponentialRampToValueAtTime(volume, context.currentTime + 2);
+    const minsToPlay = parseInt(document.getElementById("timerMins").value, 10);
     timer(minsToPlay);
+
+    playing = true;
     document.getElementById("startButton").innerHTML = "Stop";
     document.getElementById("timerMins").disabled = true;
 }
@@ -83,19 +62,19 @@ function togglePlay() {
 async function stop() {
     document.getElementById("startButton").disabled = true;
     setCountdownText("--", "--");
-    clearInterval(this.timerId);
+    clearInterval(timerId);
 
     const fadeDuration = 3;
-    this.gain.gain.setValueAtTime(this.volume, context.currentTime);
-    this.gain.gain.exponentialRampToValueAtTime(
+    gain.gain.setValueAtTime(volume, context.currentTime);
+    gain.gain.exponentialRampToValueAtTime(
         0.001,
         context.currentTime + fadeDuration
     );
     const delay = (ms) => new Promise((res) => setTimeout(res, ms));
     await delay(fadeDuration * 1000);
-    this.osc1.stop();
-    this.osc2.stop();
-    this.playing = false;
+    osc1.stop();
+    osc2.stop();
+    playing = false;
 
     document.getElementById("startButton").innerHTML = "Start";
     document.getElementById("startButton").disabled = false;
@@ -104,32 +83,29 @@ async function stop() {
 }
 
 function updateFrequency(newFreq, oscillatorId) {
-    var correctedFreq = Math.min(Math.max(parseInt(newFreq), 1), 1000);
+    const correctedFreq = Math.min(Math.max(parseInt(newFreq), 1), 1000);
     if (oscillatorId === 1) {
-        if (this.playing === true) this.osc1.frequency.value = correctedFreq;
+        if (playing === true) osc1.frequency.value = correctedFreq;
         document.getElementById("frequencyText1").value = correctedFreq;
         document.getElementById("freq1").value = correctedFreq;
     } else if (oscillatorId === 2) {
-        if (this.playing === true) this.osc2.frequency.value = correctedFreq;
+        if (playing === true) osc2.frequency.value = correctedFreq;
         document.getElementById("frequencyText2").value = correctedFreq;
         document.getElementById("freq2").value = correctedFreq;
     }
 }
 
 function updateVolume(newVolume) {
-    this.volume = newVolume / 100;
-    if (typeof this.gain !== "undefined") {
-        this.gain.gain.linearRampToValueAtTime(
-            this.volume,
-            context.currentTime + 0.1
-        );
+    volume = newVolume / 100;
+    if (!!gain) {
+        gain.gain.linearRampToValueAtTime(volume, context.currentTime + 0.1);
     }
 }
 
 function timer(mins) {
     var countdownSeconds = mins * 60;
     setCountdownTextFromSeconds(countdownSeconds);
-    this.timerId = setInterval(function () {
+    timerId = setInterval(function () {
         countdownSeconds -= 1;
         if (countdownSeconds <= 0) {
             stop();
@@ -140,8 +116,8 @@ function timer(mins) {
 }
 
 function setCountdownTextFromSeconds(countdownSeconds) {
-    var min = padNumberWithZero(countdownSeconds / 60);
-    var sec = padNumberWithZero(countdownSeconds - min * 60);
+    const min = padNumberWithZero(countdownSeconds / 60);
+    const sec = padNumberWithZero(countdownSeconds - min * 60);
     setCountdownText(min, sec);
 }
 
